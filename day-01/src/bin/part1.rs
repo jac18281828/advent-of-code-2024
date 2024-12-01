ab961d27744f74f3b2795df5b946fcc0970c9383
John Cairns <john@2ad.com>
2024-12-01 16:50:13 +0000

    fix: add tests and formatting

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,13 +1,10 @@
-use std::io;
 use itertools::izip;
+use std::io;
 
-fn main() {
+fn parse_lines_and_sum_differences(lines: &Vec<String>) -> i32 {
     let mut l1: Vec<i32> = vec![];
     let mut l2: Vec<i32> = vec![];
-    let lines = io::stdin()
-        .lines()
-        .map(|line| line.unwrap())
-        .collect::<Vec<String>>();
+
     lines.iter().for_each(|line| {
         let parts = line.split_ascii_whitespace().collect::<Vec<&str>>();
         l1.push(parts[0].parse::<i32>().unwrap());
@@ -19,5 +16,43 @@ fn main() {
     for (x1, x2) in izip!(l1.iter(), l2.iter()) {
         sum_distance = sum_distance + (x1 - x2).abs();
     }
+    sum_distance
+}
+
+fn main() {
+    let lines = io::stdin()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+    let sum_distance = parse_lines_and_sum_differences(&lines);
     println!("{}", sum_distance);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_lines_and_sum_differences() {
+        let lines = vec![
+            "1 8".to_string(),
+            "3 6".to_string(),
+            "5 4".to_string(),
+            "7 2".to_string(),
+        ];
+        assert_eq!(parse_lines_and_sum_differences(&lines), 4);
+    }
+
+    #[test]
+    fn test_parse_lines_and_sum_differences_2() {
+        let lines = vec![
+            "3   4".to_string(),
+            "4   3".to_string(),
+            "2   5".to_string(),
+            "1   3".to_string(),
+            "3   9".to_string(),
+            "3   3".to_string(),
+        ];
+        assert_eq!(parse_lines_and_sum_differences(&lines), 11);
+    }
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,12 +1,8 @@
 use std::io;
 
-fn main() {
+fn parse_lines_and_sum_similarity_score(lines: &Vec<String>) -> i32 {
     let mut l1: Vec<i32> = vec![];
     let mut count_map = std::collections::HashMap::new();
-    let lines = io::stdin()
-        .lines()
-        .map(|line| line.unwrap())
-        .collect::<Vec<String>>();
     lines.iter().for_each(|line| {
         let parts = line.split_ascii_whitespace().collect::<Vec<&str>>();
         let x1 = parts[0].parse::<i32>().unwrap();
@@ -21,5 +17,43 @@ fn main() {
         let count = count_map.get_mut(&x1).unwrap();
         sum_similarity = sum_similarity + *count * x1;
     }
+    sum_similarity
+}
+
+fn main() {
+    let lines = io::stdin()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+    let sum_similarity = parse_lines_and_sum_similarity_score(&lines);
     println!("{}", sum_similarity);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_lines_and_sum_similarity_score() {
+        let lines = vec![
+            "1 8".to_string(),
+            "3 6".to_string(),
+            "5 4".to_string(),
+            "7 2".to_string(),
+        ];
+        assert_eq!(parse_lines_and_sum_similarity_score(&lines), 0);
+    }
+
+    #[test]
+    fn test_parse_lines_and_sum_similarity_score_2() {
+        let lines = vec![
+            "3   4".to_string(),
+            "4   3".to_string(),
+            "2   5".to_string(),
+            "1   3".to_string(),
+            "3   9".to_string(),
+            "3   3".to_string(),
+        ];
+        assert_eq!(parse_lines_and_sum_similarity_score(&lines), 31);
+    }
 }
