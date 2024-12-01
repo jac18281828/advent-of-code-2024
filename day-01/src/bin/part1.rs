49d49c7020688a5384d658c766b53bbbc2598b72
John Cairns <john@2ad.com>
2024-12-01 14:11:48 +0000

    chore: part1 correct

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,23 @@
+use std::io;
+use itertools::izip;
+
 fn main() {
-    println!("Hello, world!");
+    let mut l1: Vec<i32> = vec![];
+    let mut l2: Vec<i32> = vec![];
+    let lines = io::stdin()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+    lines.iter().for_each(|line| {
+        let parts = line.split_ascii_whitespace().collect::<Vec<&str>>();
+        l1.push(parts[0].parse::<i32>().unwrap());
+        l2.push(parts[1].parse::<i32>().unwrap());
+    });
+    l1.sort();
+    l2.sort();
+    let mut sum_distance = 0;
+    for (x1, x2) in izip!(l1.iter(), l2.iter()) {
+        sum_distance = sum_distance + (x1 - x2).abs();
+    }
+    println!("{}", sum_distance);
 }
